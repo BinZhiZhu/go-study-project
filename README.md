ebefef44dd6ba64a158d24a5cee0b0ba5c9c787e
bin <854767470@qq.com>
2021-08-11 23:46:27 +0800

    feat(update): 切片、异常处理、range遍历、map映射、函数闭包

--- README.md
+++ README.md
@@ -4,10 +4,14 @@
 
 https://tour.go-zh.org/
 
-## PLAN
+## TODO LIST
 
-~~1、基础语法 2021-08-08  done~~  
-~~2、流程控制 2021-08-09  done~~
+~~1、基础语法 2021-08-08~~  
+~~2、流程控制 2021-08-09~~  
+~~3、指针、结构体struct、数组、切片使用、range循环遍历~~  
+
+## DOC
+1、字符串 strings.Fields https://go-zh.org/pkg/strings/#Fields
 
 ## author
 
--- moretypes.go
+++ moretypes.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"time"
+	"strings"
 )
 
 func ending() {
 	fmt.Println("更多类型章节学习结束...", time.Now().String())
 }
 
+//昵称
+type NicknameObj struct {
+	name string
+}
 //指针问题
 func pointer()  {
 	i,j :=10,20
@@ -35,6 +40,21 @@ type memberInfo struct {
 	avatar string
 }
 
+func catchError()  {
+	if err := recover(); err != nil {
+		fmt.Println("捕获到了panic产生的异常：", err) // 这里的err其实就是panic传入的内容
+	}
+}
+
+func adder() func(int) int  {
+	sum :=1
+	return func(i int) int {
+		fmt.Println("i===",i,sum)
+		return i+sum;
+	}
+}
+
+//Go中引入的Exception处理：defer, panic, recover
 func main() {
 	fmt.Println("更多类型学习...", time.Now().String())
 	defer ending()
@@ -116,4 +136,116 @@ func main() {
 	//上界肯定是最大长度
 	s = s[1:]
 	fmt.Println(s)
+
+	//nil 切片
+	var strArr []string;
+	fmt.Println(strArr,len(strArr),cap(strArr))
+	if strArr == nil{
+		fmt.Println("strArr is nil")
+	}
+
+	//make创建切片
+	//创建长度=容量都为5的切片
+	emptySlice := make([]int,5)
+	fmt.Println(emptySlice)
+	//创建长度=2，容量为5的切片
+	slice1 :=make([]string,2,5)
+	fmt.Println(slice1,len(slice1),cap(slice1))
+	slice1[1] = "Nikita"
+
+	// 必须要先声明defer，否则不能捕获到panic异常,也就是说要先注册函数，后面有异常了，才可以调用
+	defer catchError()
+
+	//报错，因为容量最多为5，不能超出
+	//slice1[5] = "Nikita1"
+	//fmt.Println(slice1)
+
+	// 创建一个井字板（经典游戏）
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
+	fmt.Println(board)
+
+	//向切片追加元素:func append(s []T, vs ...T) []T
+	//空切片
+	slice2 :=make([]int,0)
+	fmt.Println(slice2)
+	slice2 =append(slice2,1,2)
+	fmt.Println(slice2)
+
+	//range使用
+	nicknames :=[] string{"Nikita","Peter","pony"}
+	fmt.Println(nicknames)
+	//循环遍历nicknames
+	for i,v :=range nicknames{
+		fmt.Println(i,v)
+	}
+	numberData :=make([]int,0);
+	numberData = append(numberData, 1,2,3,4,5)
+	for i,v :=range numberData{
+		fmt.Println(i,v)
+	}
+	//忽略value，只遍历key
+	for key :=range numberData{
+		fmt.Println(key)
+	}
+	//忽略key，只遍历value
+	for _,value :=range numberData{
+		fmt.Println("value->",value)
+	}
+
+
+	// 映射map: 其实就是类型定义一个数组，数组key=>对象
+	var cpNickname map[string]NicknameObj
+	cpNickname =make(map[string]NicknameObj)
+	fmt.Println(cpNickname)
+	cpNickname["name"] = NicknameObj{name: "哈哈"}
+	fmt.Println(cpNickname,cpNickname["name"])
+
+	//定义一个映射文法，people对应的信息
+	students := map[string]memberInfo{
+		"Nikita":memberInfo{
+			id: 1,
+			nickname: "Nikita",
+			avatar: "",
+		},
+		"Peter":{id: 2,nickname: "Peter",avatar: ""},
+	}
+	fmt.Println(students)
+
+	//删除学生Peter
+	delete(students,"Peter")
+	fmt.Println(students)
+
+	//检查peter是否存在
+	v,hasPeter := students["Peter"]
+	fmt.Println(v,hasPeter,students["Peter"] == memberInfo{})
+	c :=strings.Fields("I am learning Go!")
+	fmt.Println(c)
+	for i,v :=range c{
+		fmt.Println(i,v)
+	}
+
+	//函数闭包
+	pos := adder()
+
+	pos(0)
+
+	var aName []string
+	aName = append(aName,"sss")
+	strings.Join(aName,"")
+	fmt.Println(aName)
+	str := "12"
+	str += "test-string"
+	fmt.Println(str)
+
+	str = strings.Join([]string{str, "-test-xxx"}, "")
+	fmt.Println(str)
+
+	//https://go-zh.org/pkg/strings/#Fields
+	//抛出异常:Go中引入的Exception处理：defer, panic, recover
+	panic("抛出异常xxx")//直接终止程序
+	fmt.Println("error")
 }
